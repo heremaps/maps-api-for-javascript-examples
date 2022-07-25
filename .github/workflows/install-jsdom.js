1733c9f39ded4724eef3be44b97f1e3ca6af0a9e
Nele Gross <nele.gross@here.com>
2022-07-25 19:33:40 +0200

    install jsdom

--- .github/workflows/install-jsdom.js
+++ .github/workflows/install-jsdom.js
@@ -1,6 +1,15 @@
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+// const { JSDOM } = node-jsdom;
 
 
-const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
-console.log(dom.window.document.querySelector("p").textContent); // "Hello world"
+// const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+// console.log(dom.window.document.querySelector("p").textContent); // "Hello world"
+
+var jsdom = require("node-jsdom");
+ 
+jsdom.env(
+  "http://nodejs.org/dist/",
+  ["http://code.jquery.com/jquery.js"],
+  function (errors, window) {
+    console.log("there have been", window.$("a").length, "nodejs releases!");
+  }
+);
