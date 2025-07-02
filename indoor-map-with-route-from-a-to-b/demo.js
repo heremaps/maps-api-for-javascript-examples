0ce647302a0477075f0c4bd367bb1a29395e7fbd
Ravi Singh <ravi.u.singh@here.com>
2025-07-02 15:07:02 +0530

    updated meta.json

--- indoor-map-with-route-from-a-to-b/demo.js
+++ indoor-map-with-route-from-a-to-b/demo.js
@@ -2,29 +2,26 @@
  * Example for Indoor Map for JSMapsApi.
  */
 
-// Replace with your HERE platform app api key
-const yourApikey = 'ZKBUeAgkzH4JWhg93AA7cIE_kZotbMGhVI0_UYC0COY';
-
 // Replace with your indoor map platform collection hrn
-const indoorMapHrn = 'hrn:here:data::org651595200:indoormap-ed6d5667-cfe0-4748-bbf5-88b00e7e3b21-collection';
+var indoorMapHrn = 'hrn:here:data::org651595200:indoormap-ed6d5667-cfe0-4748-bbf5-88b00e7e3b21-collection';
 
-const venuesProvider = null
-const venuesService = null
+var venuesProvider = null
+var venuesService = null
 // Specify the venue ID for your map. Examples of the map ID mentioned below.
 // For legacy maps, you can continue to use the numeric value. 
 // Examples:
 // indoormap-00000000-0000-4000-a000-000000007348 for Zurich Airport (legacy id 7348) 
 // indoormap-00000000-0000-4000-a000-000000027158 for Tiefgarage Riem Arcaden APCOA Parking garage (legacy id 27158) 
 // indoormap-00000000-0000-4000-a000-000000022766 for Mall of Berlin (legacy id 22766)
-const venueId = 'indoormap-00000000-0000-4000-a000-000000007348';
+var venueId = 'indoormap-00000000-0000-4000-a000-000000022766';
 
 // Set to false if base map is not needed to be displayed.
-const showBaseMap = true;
+var showBaseMap = true;
 
 // Optional, list of label text preferences to override.
-const labelTextPreferenceOverride = [
-  H.venues.Service2.LABEL_TEXT_PREFERENCE_OVERRIDE.OCCUPANT_NAMES,
-  H.venues.Service2.LABEL_TEXT_PREFERENCE_OVERRIDE.SPACE_NAME
+var labelTextPreferenceOverride = [
+  H.venues.Service.LABEL_TEXT_PREFERENCE_OVERRIDE.OCCUPANT_NAMES,
+  H.venues.Service.LABEL_TEXT_PREFERENCE_OVERRIDE.SPACE_NAME
 ]
 
 /**
@@ -38,14 +35,8 @@ function addVenueToMap(map, platform) {
   venuesProvider = new H.venues.Provider();
 
   // Get an instance of the Indoor Maps service using a valid apikey for Indoor Maps
-  venuesService = platform.getVenuesService({ apikey: yourApikey, hrn: indoorMapHrn }, 2);
-
-  // Use venuesService.getMapInfoList to retrieve the list of Indoor maps from the given HRN
-  venuesService.getMapInfoList().then(mapInfoList => {
-    mapInfoList.forEach(mapInfo => {
-      console.log("Indoor map id: " + mapInfo.mapId + ", map name: " + mapInfo.mapName);
-    });
-  });
+  // In your own code, replace variable "yourToken" with your own token to access indoor routing. 
+  venuesService = platform.getVenuesService({ token: "yourToken", hrn: indoorMapHrn });
 
   // Indoor Maps service provides a loadVenue method. Optionally, overriding the label preferences
   venuesService.loadVenue(venueId, { labelTextPreferenceOverride }).then((venue) => {
@@ -54,7 +45,7 @@ function addVenueToMap(map, platform) {
     venuesProvider.setActiveVenue(venue);
 
     // create a tile layer for the Indoor Maps provider
-    const venueLayer = new H.map.layer.TileLayer(venuesProvider);
+    var venueLayer = new H.map.layer.TileLayer(venuesProvider);
     if (showBaseMap) {
       // Add venueLayer to the base layer
       map.addLayer(venueLayer);
@@ -67,11 +58,11 @@ function addVenueToMap(map, platform) {
     map.setCenter(venue.getCenter());
 
     // Create a level control
-    const levelControl = new H.venues.ui.LevelControl(venue);
+    var levelControl = new H.venues.ui.LevelControl(venue);
     ui.addControl('level-control', levelControl);
 
     // Create a drawing control:
-    const drawingControl = new H.venues.ui.DrawingControl(venue);
+    var drawingControl = new H.venues.ui.DrawingControl(venue);
     ui.addControl('drawing-control', drawingControl);
   });
 }
@@ -86,11 +77,11 @@ function addVenueToMap(map, platform) {
 async function calculateIndoorRouteFromAtoB(map) {
 
   // Instantiate (and display) a map:
-  const origin = { lat: 47.45146718297871, lng: 8.56116163852909, levelId: "level-9050", venueId: "indoormap-00000000-0000-4000-a000-000000007348" };
-  const destination = { lat: 47.45264049318705, lng: 8.562764022046945, levelId: "level-9050", venueId: "indoormap-00000000-0000-4000-a000-000000007348" };
+  var origin = { lat: 47.45146718297871, lng: 8.56116163852909, levelId: "level-9050", venueId: "indoormap-00000000-0000-4000-a000-000000007348" };
+  var destination = { lat: 47.45264049318705, lng: 8.562764022046945, levelId: "level-9050", venueId: "indoormap-00000000-0000-4000-a000-000000007348" };
 
   // Create the parameters for the indoor routing request:
-  const routingParameters = {
+  var routingParameters = {
     routingMode: "fast",
     transportMode: "pedestrian",
     // The start point of the route:
@@ -99,17 +90,17 @@ async function calculateIndoorRouteFromAtoB(map) {
     destination: `${destination.lat},${destination.lng},${destination.levelId},${destination.venueId}`,
   };
   try{
-    const res = await venuesService.calculateRoute(routingParameters);
+    var res = await venuesService.calculateRoute(routingParameters);
     if (res?.routes && res.routes.length > 0) {
       // Create route objects using route response data
-      const route = new H.venues.Route2(res.routes[0]);
+      var route = new H.venues.Route(res.routes[0]);
       // Link route map objects with venue levels for automatic visibility updates:
-      const indoorObjects = route.getIndoorObjects();
+      var indoorObjects = route.getIndoorObjects();
 
       for (let venueId in indoorObjects) {
         for (let levelIndex in indoorObjects[venueId]) {
-          const venue = venuesProvider.getVenue(venueId);
-          const objectGroup = indoorObjects[venueId][levelIndex];
+          var venue = venuesProvider.getVenue(venueId);
+          var objectGroup = indoorObjects[venueId][levelIndex];
           map.addObject(objectGroup);
           venue.setMapObjects(objectGroup.getObjects(), levelIndex);
         }
@@ -127,7 +118,7 @@ async function calculateIndoorRouteFromAtoB(map) {
 // Step 1: initialize communication with the platform
 // In your own code, replace variable window.apikey with your own apikey
 var platform = new H.service.Platform({
-  apikey: yourApikey
+  apikey: window.apikey
 });
 var defaultLayers = platform.createDefaultLayers();
 
